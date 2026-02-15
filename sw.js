1174477e11cab0af5f8b709a5e65a88997428060
Open Claw <openclaw@Opens-Mac-mini.local>
2026-02-15 00:37:47 -0800

    Update dashboards 2026-02-15T08:37:47Z

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'jarvis-dash-v1';
+const ASSETS = [
+  './',
+  './dashboard_index.html',
+  './dashboard_crypto.html',
+  './dashboard_football.html',
+  './dashboard_multiasset.html',
+  './dashboard_exchange.html',
+  './dashboard_arb.html',
+  './dashboard_backtests.html',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+});
